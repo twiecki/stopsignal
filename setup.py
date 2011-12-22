61fc87849fe438b209b365dbb71c97114ea37b0d
doramatzke <d.matzke@uva.nl>
2011-12-22 18:04:44 +0100

    Fixed gsl setup on windows

--- setup.py
+++ setup.py
@@ -4,14 +4,8 @@ from Cython.Distutils import build_ext
 import numpy as np
 import os
 import sys
-print sys.platform
 
-if sys.platform == "win32":
-    gsl_include = r"c:\Program Files\GnuWin32\include"
-    lib_gsl_dir = r"c:\Program Files\GnuWin32\lib"
-else:
-    gsl_include = os.popen('gsl-config --cflags').read()[2:-1]
-    lib_gsl_dir = ''
+gsl_include = os.popen('gsl-config --cflags').read()[2:-1]
 
 if gsl_include == '':
     print "Couldn't find gsl-config. Make sure it's installed and in the path."
@@ -30,7 +24,7 @@ setup(
     install_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
     setup_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
     include_dirs = [np.get_include(), gsl_include],
-    library_dirs=[lib_gsl_dir],
+    cmdclass = {'build_ext': build_ext},
     classifiers=[
                 'Development Status :: 3 - Alpha',
                 'Environment :: Console',
@@ -40,5 +34,5 @@ setup(
                 'Programming Language :: Python',
                 'Topic :: Scientific/Engineering',
                  ],
-    ext_modules = [Extension("stop_likelihoods", ["src/stop_likelihoods.c"], libraries=['gsl','gslcblas'])]
+    ext_modules = [Extension("stop_likelihoods", ["src/stop_likelihoods.pyx"], libraries=['gsl','gslcblas'])]
 )
