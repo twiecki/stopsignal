c1c3901130f97e1f29c04d4fdf5bece6aeedb18b
Thomas Wiecki <thomas.wiecki@gmail.com>
2011-12-22 12:06:32 +0100

    Added setup file. Restructured directories.

---
+++ setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+import os
+import sys
+
+gsl_include = os.popen('gsl-config --cflags').read()[2:-1]
+
+if gsl_include == '':
+    print "Couldn't find gsl-config. Make sure it's installed and in the path."
+    sys.exit(-1)
+
+setup(
+    name="stopsignal",
+    version="0.1a",
+    author="Thomas V. Wiecki, Dora Matzke, Eric-Jan Wagenmakers",
+    author_email="thomas_wiecki@brown.edu",
+    url="http://github.com/twiecki/stopsignal",
+    packages=["stopsignal"],
+    package_data={"stopsignal":["examples/*.csv"]},
+    description="""stopsignal implements a hierarchical Bayesian estimation of the stopsignal model presented in Matzke et al(2011) in kabuki.
+    Matzke, D., Dolan, C.V, Logan, G.D., Brown, S.D., & Wagenmakers, E.-J. (2011). Bayesian parametric estimation of stop-signal reaction time distributions. Manuscript submitted for publication.""",
+    install_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
+    setup_requires=['NumPy >=1.3.0', 'kabuki >= 0.2a', 'pymc'],
+    include_dirs = [np.get_include()],
+    cmdclass = {'build_ext': build_ext},
+    classifiers=[
+                'Development Status :: 3 - Alpha',
+                'Environment :: Console',
+                'Operating System :: OS Independent',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: GNU General Public License (GPL)',
+                'Programming Language :: Python',
+                'Topic :: Scientific/Engineering',
+                 ],
+    ext_modules = [Extension("stop_likelihoods", ["src/stop_likelihoods.pyx"])]
+)
