1924d9ee6736a8a30ccc380dc085e762b334e1bf
Thomas Wiecki <thomas.wiecki@gmail.com>
2012-11-01 15:19:07 -0400

    FIX: stopsignal_conv.py many smaller fixes of making it work.

--- stopsignal/stopsignal_conv.py
+++ stopsignal/stopsignal_conv.py
@@ -73,35 +73,43 @@ class StopSignal(Hierarchical):
 
     def create_ss_knode(self, knodes):
         ss_parents = OrderedDict()
-        ss_parents['mu_go'] = knodes['mu_go_bottom']
-        ss_parents['sigma_go'] = knodes['sigma_go_bottom']
-        ss_parents['tau_go'] = knodes['tau_go_bottom']
-        ss_parents['mu_stop'] = knodes['mu_stop_bottom']
-        ss_parents['sigma_stop'] = knodes['sigma_stop_bottom']
-        ss_parents['tau_stop'] = knodes['tau_stop_bottom']
+        # TW: The keys in the ss_parents dict have to the names of the arguments you use in the likelihoods (prefixed with an 'i')
+        ss_parents['imu_go'] = knodes['mu_go_bottom']
+        ss_parents['isigma_go'] = knodes['sigma_go_bottom']
+        ss_parents['itau_go'] = knodes['tau_go_bottom']
+        ss_parents['imu_stop'] = knodes['mu_stop_bottom']
+        ss_parents['isigma_stop'] = knodes['sigma_stop_bottom']
+        ss_parents['itau_stop'] = knodes['tau_stop_bottom']
 
         #go_like = KnodeGo(Go_like, 'go_like', imu_go = 'mu_go', isigma_go = 'sigma_go', itau_go = 'tau_go', col_name='rt', observed=True)
         #srrt_like = KnodeSRRT(SRRT_like, 'srrt_like', imu_go = 'mu_go', isigma_go = 'sigma_go', itau_go = 'tau_go', imu_stop = 'mu_stop', isigma_stop = 'sigma_stop',itau_stop = 'tau_stop', col_name='rt', observed=True)
         #inhibitions_like = KnodeInhibitions(Inhibitions_like, 'inhibitions_like',  imu_go = 'mu_go', isigma_go = 'sigma_go', itau_go = 'tau_go', imu_stop = 'mu_stop', isigma_stop = 'sigma_stop', itau_stop = 'tau_stop', col_name='rt', observed=True)
-        go_like = KnodeGo(Go_like, 'go_like', col_name='rt', observed=True, imu_go = 'mu_go', isigma_go = 'sigma_go', itau_go = 'tau_go')
-        srrt_like = KnodeSRRT(SRRT_like, 'srrt_like', col_name='rt', observed=True,**ss_parents)
-        inhibitions_like = KnodeInhibitions(Inhibitions_like, 'inhibitions_like',col_name='rt', observed=True,**ss_parents)
-             
+        # TW: This was a bug, you passed strings before but you need to pass the knode (which is an element in the ss_parents dictionary).
+        go_like = KnodeGo(Go_like, 'go_like', col_name='rt', observed=True, imu_go=ss_parents['imu_go'], isigma_go=ss_parents['isigma_go'], itau_go=ss_parents['itau_go'])
+        srrt_like = KnodeSRRT(SRRT_like, 'srrt_like', col_name='rt', observed=True, **ss_parents)
+        inhibitions_like = KnodeInhibitions(Inhibitions_like, 'inhibitions_like', col_name='rt', observed=True, **ss_parents)
+
         return [go_like,srrt_like,inhibitions_like]
 
     def create_knodes(self):
         knodes = OrderedDict()
-        knodes.update(self.create_family_trunc_normal('mu_go', lower=1e3, upper=1e3, value=500,var_lower=0.01, var_upper=300, var_value=50))
+        knodes.update(self.create_family_trunc_normal('mu_go', lower=1e-3, upper=1e3, value=500,var_lower=0.01, var_upper=300, var_value=50))
         knodes.update(self.create_family_trunc_normal('sigma_go', lower=1, upper=300, value=50,var_lower=0.01, var_upper=200, var_value=20))
         knodes.update(self.create_family_trunc_normal('tau_go', lower=1, upper=300, value=40,var_lower=0.01, var_upper=200, var_value=20))
         knodes.update(self.create_family_trunc_normal('mu_stop', lower=1e-3, upper=600, value=200,var_lower=0.01, var_upper=200, var_value=40))
         knodes.update(self.create_family_trunc_normal('sigma_stop', lower=1, upper=250, value=30,var_lower=0.01, var_upper=100, var_value=20))
         knodes.update(self.create_family_trunc_normal('tau_stop', lower=1, upper=250, value=30,var_lower=0.01, var_upper=100, var_value=20))
-        #what is value/ var_value?-->start values? 
+        #what is value/ var_value?-->start values?
+        # TW: yes. starting value for mean and var_value is starting value for variance nodes.
         #Can I generate uniform random numbers instead of giving it a fixed value? How to set start values for the individual subject parameters?
+        # TW: you should be able to use np.random.rand() and supply that to as value
         #In WinBUGS implementation, group-level means (g) are (truncated) normally distributed; here they are Uniform (in hierarchical.py).
         #Is it possible to overwrite that?
-        knodes['go_like','srrt_like','inhibitions_like'] = self.create_ss_knode(knodes)#What does this do exactly? Can I pass on the likleihoods like that? 
+        # TW: You would need to create your own create_family convenience function (see the code, it's basically doing the same thing as you are doing in stopsignal.py by hand).
+        # TW: I would consider reformulating the model however because truncated normal distributions are not as easy to sample from and you can't use gibbs sampling. What we did in the new HDDM version was to use Normal group distributions and Normal subject distributions and then transform them appropriately (e.g. exp if values are positive; for this you can use the convenience function create_family_exp(). See hddm.models.hddm_transformed.py for an example). This made convergence so much better and faster! Especially because convergence in the examples I ran is not great.
 
-        return knodes.values()
-    
+        likelihoods = self.create_ss_knode(knodes)#What does this do exactly? Can I pass on the likleihoods like that?
+        # TW: We just need to return the knodes. I save them in a dictionary above so that I can fetch them out in create_ss_knode(), but Hierarchical just requires a list so we can just save your likelihood knodes and return them together with the stochastic knodes.
+
+        return knodes.values() + likelihoods
+
